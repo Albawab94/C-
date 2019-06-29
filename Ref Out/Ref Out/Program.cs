50f0e4b664e803728c42df92cf806f1f67dbc29e
Albawab <albawab94@gmail.com>
2019-06-29 10:36:54 +0200

    Ref Out

---
+++ Ref Out/Ref Out/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_Out
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            int i;
+            int m = 5 ;
+            RefOut(out i);
+            Console.WriteLine("this is the out "+i);
+
+            Ref(ref m);
+            Console.WriteLine("This is the Ref " + m);
+            Console.ReadKey();
+        }
+
+        public static void RefOut(out int ut)
+        {
+            ut = 5;
+        }
+
+        public static void Ref(ref int i)
+        {
+            i += 5;
+        }
+    }
+}
